fc85d018d84f71b3ed471fac00eb4efb4fa1b7d4
naiaraAM <nam312@alumnos.unican.es>
2024-05-19 11:25:57 +0200

    updates

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+sudo apt install python3-django
+sudo apt install python3-magic
+sudo apt install python3-matplotlib
+sudo apt install python3-pandas
+sudo apt install python3-textdistance
+sudo apt install pev -y
--- instructions/install.sh
+++
@@ -1,8 +0,0 @@
-sudo apt install git -y # si no está
-`sudo apt install python3-pip -y`
-`sudo apt install python3-django`
-`sudo apt install python3-magic`
-`sudo apt install python3-matplotlib`
-`sudo apt install pev -y`
-`sudo apt install python3-pandas`
-`sudo apt install python3-textdistance`
